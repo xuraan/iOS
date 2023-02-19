1a0edd3330f36b4797c910c198ac71ca5851dcf2
afrcvn <afrcvn@gmail.com>
2023-02-19 02:41:06 -0500

    setUp new version

--- Quran/ContentView.swift
+++ Quran/ContentView.swift
@@ -9,35 +9,11 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) var viewContext
-    @FetchRequest var favorite: FetchedResults<Kollection>
-    @StateObject var model: Model = .init()
-    @StateObject var quranVM: QuranViewModel = .init()
-    @StateObject var suraVM: SuraViewModel = .init()
-    @StateObject var searchVM: SearchModel = .init()
-    @StateObject var ayaVM: AyaViewModel = .init()
-    @State var pinned: Any? = nil
-    init(){
-        _favorite = Kollection.request(for: "C6819E4A-9203-48CE-9EE4-AAF815B52D09")
-    }
+    
     var body: some View {
-        MainView()
-        //MARK: - ENVIRONMENTS OBJECT
-        .environmentObject(model)
-        .environmentObject(quranVM)
-        .environmentObject(suraVM)
-        .environmentObject(searchVM)
-        .environmentObject(ayaVM)
-        .environment(\.favorite, favorite.first)
-        .environment(\.pinned, $pinned)
-        .alert("Error", isPresented: .constant(!ErrorHandler.shared.errorMessage.isEmpty), actions: {
-            Button(role: .cancel, action: {ErrorHandler.shared.errorMessage = ""}, label:{Text("OK")})
-        }, message: {
-            Text(ErrorHandler.shared.errorMessage)
-        })
-        //MARK: -THEME
-        .preferredColorScheme(model.preferredColorScheme)
-        
+        NavigationStack{
+            
+        }
     }
 }
 
@@ -45,6 +21,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
