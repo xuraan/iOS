6374eb1bb082cb0418f8adf2fdd7524c2092e97a
afrcvn <afrcvn@gmail.com>
2023-02-20 11:43:16 -0500

    create AyasCard

---
+++ Quran/Quran/Aya/AyasCard.swift
@@ -0,0 +1,68 @@
+//
+//  AyasCard.swift
+//  Quran
+//
+//  Created by Samba Diawara on 2023-02-20.
+//
+
+import SwiftUI
+
+struct AyasCard: View {
+    var header: AnyView
+    var footer: AnyView
+    let ayas: [Aya]
+    
+    init<Header: View>(_ ayas: [Aya], @ViewBuilder header: @escaping ()-> Header) {
+        self.header = AnyView(header())
+        footer = AnyView(EmptyView())
+        self.ayas = ayas
+    }
+    
+    init<Footer: View>(ayas: [Aya], @ViewBuilder footer: @escaping ()-> Footer) {
+        self.footer = AnyView(footer())
+        header = AnyView(EmptyView())
+        self.ayas = ayas
+    }
+    
+    init<Header: View, Footer: View>(
+        _ ayas: [Aya],
+        @ViewBuilder header: @escaping ()-> Header,
+        @ViewBuilder footer: @escaping ()-> Footer
+    ) {
+        self.footer = AnyView(footer())
+        self.header = AnyView(header())
+        self.ayas = ayas
+    }
+    
+    var body: some View {
+        VStack{
+            header
+                .lineLimit(1)
+                .font(.footnote.bold())
+                .foregroundColor(.secondary)
+
+            Text(ayas.map{ $0.textWithEndAya }.joined(separator: " "))
+                .environment(\.layoutDirection, .rightToLeft)
+                .font(CustomFont.mequran(22))
+                .lineLimit(3)
+            
+            Text(ayas.map{ $0.translation }.joined(separator: " "))
+                .environment(\.layoutDirection, .leftToRight)
+                .foregroundColor(.secondary)
+                .italic()
+                .lineLimit(3)
+            
+            footer
+                .lineLimit(1)
+                .font(.footnote.bold())
+                .foregroundColor(.secondary)
+                .offset(y: 10)
+        }
+        .environment(\.colorScheme, .dark)
+        .padding()
+        .listRowInsets(EdgeInsets())
+        .listRowBackground(Color.accentColor)
+        .background(Color.accentColor)
+    }
+}
+
